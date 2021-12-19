903d1c59358ac3b0e219a87f90e532678887c007
jsimonrichard <jsimonrichard@gmail.com>
2021-12-19 17:44:38 -0500

    Fix send welcome email function

--- app/functions/sendWelcomeEmail.js
+++ app/functions/sendWelcomeEmail.js
@@ -6,7 +6,7 @@ exports = function(authEvent) {
     const courier = CourierClient({ authorizationToken: context.values.get("courierAuthToken") });
 
     // Send welome message
-    const { messageId } = await courier.send({
+    courier.send({
       brand: "84A0QBW8DYMGG5N9M0P2ZX8Y6DPW",
       eventId: "Y5FFEN3CXVM0AHKN2WHMQ2A7TGMY",
       recipientId: "d2951d4e-0a24-4091-8965-9870c1ff6ab8",
@@ -15,9 +15,11 @@ exports = function(authEvent) {
       },
       data: {},
       override: {},
+    }).then(function({messageId}) {
+      // Log confirmation
+      console.log("Message "+messageId+" sent");
     });
 
-    console.log("Message "+messageId+" sent");
   } else {
 
     console.log("No email associated with user: " +authEvent.user.id);
