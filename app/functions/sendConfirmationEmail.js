4e48dcb5358a147d8cb34929fb83f45499703b48
jsimonrichard <jsimonrichard@gmail.com>
2021-12-22 23:08:31 -0500

    Separate confirmation logic

---
+++ app/functions/sendConfirmationEmail.js
@@ -0,0 +1,28 @@
+exports = async function(token, tokenId, email) {
+  // construct confirm link
+  var link = `https://${context.values.get("domainName")}/confirmEmail?token=${encodeURIComponent(token)}&tokenId=${encodeURIComponent(tokenId)}`
+  
+  // Setup courier
+  var { CourierClient } = require("@trycourier/courier");
+  const courier = CourierClient({ authorizationToken: context.values.get("courierAuthToken") });
+
+  // Send message
+  console.log(username);
+  var { messageId } = await courier.send({
+    brand: "84A0QBW8DYMGG5N9M0P2ZX8Y6DPW",
+    eventId: "C2JMC6WNHD4TC3MQA46XSQENCFJQ",
+    profile: {
+      email: email,
+    },
+    data: {
+      confirmLink: link
+    },
+    override: {},
+  });
+
+  // Log confirmation
+  console.log("Message "+messageId+" sent");
+
+  // Return status
+  return { status: 'pending' };
+};
