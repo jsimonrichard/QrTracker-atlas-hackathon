3701461bb797faf40fdd8e58287da719af79d3de
jsimonrichard <jsimonrichard@gmail.com>
2021-12-23 14:29:37 -0500

    Fix issues and build

--- frontend/src/pages/confirmEmail.js
+++ frontend/src/pages/confirmEmail.js
@@ -1,5 +1,5 @@
 import { ButtonLink } from "../components/basic";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function ConfirmEmail({ app }) {
   const urlParams = new URLSearchParams(window.location.search);
