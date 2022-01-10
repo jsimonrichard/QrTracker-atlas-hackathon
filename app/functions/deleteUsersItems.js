aa6cf6f4ff25b1af6b38041e3bc14328fd6dfbcb
jsimonrichard <jsimonrichard@gmail.com>
2022-01-10 13:43:40 -0500

    Add missing await keyword

--- app/functions/deleteUsersItems.js
+++ app/functions/deleteUsersItems.js
@@ -1,4 +1,4 @@
-exports = function(authEvent) {
+exports = async function(authEvent) {
   var db = context.services.get("mongodb-atlas").db("QrTrackerDB");
   await db.collection("tracker").deleteMany({ ownerId: context.user.id });
   await db.collection("subscription").deleteMany({ subscriberId: context.user.id });
