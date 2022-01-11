1d9728599c7eb5662bad5b4db96edde548a30815
jsimonrichard <jsimonrichard@gmail.com>
2022-01-10 19:39:42 -0500

    Fix types

--- app/functions/acceptInvitation.js
+++ app/functions/acceptInvitation.js
@@ -1,9 +1,14 @@
+/*
+This function must be executed with "System" privileges, and an authorization expression
+does not have the features nessicary to validate it correctly. For that reason a simple
+if statement within the function must be used.
+*/
+
 exports = async function(inviteId) {
   let db = context.services.get("mongodb-atlas").db("QrTrackerDB");
   let invite = await db.collection("invite").findOne({_id: BSON.ObjectId(inviteId)});
 
-  console.log(JSON.stringify(context.user.data));
-
+  // VALIDATION (DO NOT REMOVE)
   if(context.user.data.email != invite.email) {
     throw Error("This invite is not associated with the user that is currently logged in");
   }
@@ -17,7 +22,7 @@ exports = async function(inviteId) {
   );
 
   // Remove invite
-  await db.collection("invite").deleteOne({_id: inviteId});
+  await db.collection("invite").deleteOne({_id: BSON.Object(inviteId)});
 
   return invite.tracker;
 }
