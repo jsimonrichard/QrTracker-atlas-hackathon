c220ff5619bac50ad2f36b8d0a61928f6f50f29f
jsimonrichard <jsimonrichard@gmail.com>
2022-01-12 12:18:38 -0500

    Fix resolver return type

--- app/functions/latestHistoryResolver.js
+++ app/functions/latestHistoryResolver.js
@@ -1,11 +1,11 @@
 exports = async (source) => {
   let tracker_collection = context.services.get("mongodb-atlas").db("QrTrackerDB")
     .collection("tracker");
-    
+  
   let historyItem = await tracker_collection.aggregate([
     { $project: { id: 1, history: { $arrayElemAt: [ "$history", -1 ] } } },
     { $match: { _id: source._id } }
-  ]);
+  ]).next(); // Return the first item
 
   return historyItem;
 };
