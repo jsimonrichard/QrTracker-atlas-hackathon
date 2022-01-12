1a136599f76b0cbdf8cf858d2eb7177547b0e1f4
jsimonrichard <jsimonrichard@gmail.com>
2022-01-12 13:02:36 -0500

    Add parenthesis

--- app/functions/latestHistoryResolver.js
+++ app/functions/latestHistoryResolver.js
@@ -2,10 +2,10 @@ exports = async (source) => {
   let tracker_collection = context.services.get("mongodb-atlas").db("QrTrackerDB")
     .collection("tracker");
   
-  let historyItem = await tracker_collection.aggregate([
+  let historyItem = (await tracker_collection.aggregate([
     { $project: { id: 1, history: { $arrayElemAt: [ "$history", -1 ] } } },
     { $match: { _id: BSON.ObjectId("61de4643a60a6add90a32140") } }
-  ]).next().history; // Return the first item
+  ])).next().history; // Return the history of the first item
 
   return historyItem;
 };
