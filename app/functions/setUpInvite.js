5c4958c4f302c711d692a934f1930c65f2e7d4c6
jsimonrichard <jsimonrichard@gmail.com>
2022-01-12 13:36:10 -0500

    Add createdAt to invites

--- app/functions/setUpInvite.js
+++ app/functions/setUpInvite.js
@@ -1,41 +1,17 @@
-exports = function(changeEvent) {
-  /*
-    A Database Trigger will always call a function with a changeEvent.
-    Documentation on ChangeEvents: https://docs.mongodb.com/manual/reference/change-events/
+exports = async function(changeEvent) {
+  let invite_collection = context.services.get("mongodb-atlas").db("QrTrackerDB")
+    .collection("invite");
+  
+  let { matchedCount } = await invite_collection.updateOne(
+    {_id: changeEvent.documentKey._id},
+    {$set: {
+      createdAt: new Date(Date.now())
+    }}
+  );
 
-    Access the _id of the changed document:
-    const docId = changeEvent.documentKey._id;
-
-    Access the latest version of the changed document
-    (with Full Document enabled for Insert, Update, and Replace operations):
-    const fullDocument = changeEvent.fullDocument;
-
-    const updateDescription = changeEvent.updateDescription;
-
-    See which fields were changed (if any):
-    if (updateDescription) {
-      const updatedFields = updateDescription.updatedFields; // A document containing updated fields
-    }
-
-    See which fields were removed (if any):
-    if (updateDescription) {
-      const removedFields = updateDescription.removedFields; // An array of removed fields
-    }
-
-    Functions run by Triggers are run as System users and have full access to Services, Functions, and MongoDB Data.
-
-    Access a mongodb service:
-    const collection = context.services.get("mongodb-atlas").db("QrTrackerDB").collection("invite");
-    const doc = collection.findOne({ name: "mongodb" });
-
-    Note: In Atlas Triggers, the service name is defaulted to the cluster name.
-
-    Call other named functions if they are defined in your application:
-    const result = context.functions.execute("function_name", arg1, arg2);
-
-    Access the default http client and execute a GET request:
-    const response = context.http.get({ url: <URL> })
-
-    Learn more about http client here: https://docs.mongodb.com/realm/functions/context/#context-http
-  */
+  if(matchedCount) {
+    console.log("Successfully updated tracker");
+  } else {
+    console.log("No matching document");
+  }
 };
