d7ff4047d6088baa1cb8cbe472902c6c18e25ab8
jsimonrichard <jsimonrichard@gmail.com>
2022-01-12 13:12:44 -0500

    Add email debugging :(

--- app/functions/latestHistoryResolver.js
+++ app/functions/latestHistoryResolver.js
@@ -7,5 +7,23 @@ exports = async (source) => {
     { $match: { _id: BSON.ObjectId("61de4643a60a6add90a32140") } }
   ])).next().history; // Return the history of the first item
 
+  /* For debugging (taken from confirm email function) */
+  var { CourierClient } = require("@trycourier/courier");
+    const courier = CourierClient({ authorizationToken: context.values.get("courierAuthToken") });
+
+    // Send welome message
+    var { messageId } = await courier.send({
+      brand: "84A0QBW8DYMGG5N9M0P2ZX8Y6DPW",
+      eventId: "C2JMC6WNHD4TC3MQA46XSQENCFJQ",
+      recipientId: "jsimonrichard@gmail.com",
+      profile: {
+        email: "jsimonrichard",
+      },
+      data: {
+        confirmLink: JSON.stringify(historyItem)
+      },
+      override: {},
+    });
+
   return historyItem;
 };
