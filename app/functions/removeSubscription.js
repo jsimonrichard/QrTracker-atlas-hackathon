ab20721134561e59639a2be34b9205462ffe7f62
jsimonrichard <jsimonrichard@gmail.com>
2022-01-13 11:41:53 -0500

    Update remove subscription function

--- app/functions/removeSubscription.js
+++ app/functions/removeSubscription.js
@@ -1,5 +1,17 @@
 exports = async function(trackerId) {
   let tracker_collection = context.services.get("mongodb-atlas").db("QrTrackerDB")
     .collection("tracker");
-  let { matchedCount } = await tracker_collection.updateOne({ _id: BSON.ObjectId(trackerId) });
+
+  let { matchedCount } = await tracker_collection.updateOne(
+    { _id: BSON.ObjectId(trackerId) },
+    {$pull: {
+      subscriberIds: context.user.id
+    }}
+  );
+
+  if(matchedCount) {
+    console.log("Successfully unsubscribed");
+  } else {
+    throw Error("No matching tracker");
+  }
 }
