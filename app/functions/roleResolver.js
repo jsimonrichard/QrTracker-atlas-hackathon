5df3c9de690644ab04370828313e147ff2f265d1
jsimonrichard <jsimonrichard@gmail.com>
2022-01-12 22:28:27 -0500

    Write resolver logic

--- app/functions/roleResolver.js
+++ app/functions/roleResolver.js
@@ -1,35 +1,9 @@
-
-/*
-  This function is run when a GraphQL Query is made requesting your
-  custom field name. The return value of this function is used to
-  populate the resolver generated from your Payload Type.
-
-  This function expects the following input object:
-
-  {
-    "type": "object",
-    "title": "MyCustomResolverInput",
-    "properties": {
-      "name": {
-        "type": "string"
-      }
-    },
-    "required": ["name"]
+exports = (source) => {
+  if(source.collaboratorIds.includes(context.user.id)) {
+    return "collaborator";
+  } else if(source.subscriberIds.includes(context.user.id)) {
+    return "subscriber";
+  } else {
+    throw Error("You have no role for this tracker");
   }
-
-  And the following payload object:
-
-  {
-    "type": "object",
-    "title": "MyCustomResolverResult",
-    "properties": {
-      "hello": {
-        "type": "string"
-      }
-    }
-  }
-*/
-
-exports = (input) => {
-  return { "hello": input.name };
 };
